865ded79aabe0a81aed07b830e80ada39508883a
EOM <tomoru55.tom@gmail.com>
2020-12-07 21:11:28 +0900

    Updates

---
+++ precache-manifest.8c37c16af9aebdbc22ff80ade4eefedc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf93cef8e84de424a94dd3ec0248e97b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d415ddafdcafde96d9",
+    "url": "/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d415ddafdcafde96d9",
+    "url": "/static/js/main.bae183a4.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/static/media/your photo.b9d7d448.png"
+  }
+]);
